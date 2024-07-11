6437712539a0eede219dc99b86b7b4a9a152ce28
Sen Colon <tcolon316@yahoo.com>
2024-07-10 21:17:37 -0400

    End of Section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,30 @@
+﻿namespace API.Errors;
+
+public class ApiException
+{
+  private int _statusCode;
+  private string _message;
+  private string? _details;
+
+  public ApiException(int statusCode, string message, string? details)
+  {
+      _statusCode = statusCode;
+      _message = message;
+      _details = details;
+  }
+
+  public int StatusCode { 
+    get { return _statusCode; }
+    set { _statusCode = value; }
+  }
+
+  public string Message { 
+    get { return _message; }
+    set { _message = value; }
+  }
+
+  public string? Details { 
+    get { return _details; }
+    set { _details = value; }
+  }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddApplicationServies(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
